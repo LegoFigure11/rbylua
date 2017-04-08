d1a7ce9033e6339c31b1374fa91c62a84c7d1b34
LegoFigure11 <legofigurefb@gmail.com>
2017-04-08 22:33:30 +1000

    Update giftbot.lua

--- giftbot.lua
+++ giftbot.lua
@@ -3,6 +3,9 @@ local base_address
 local atkdef
 local spespc
 
+        print("RBGY Stationary Bot by zep715")
+        print("Optimised/Edited by LegoFigure11 | Available at http://spo.ink/gen1bot")
+
 if version == 0xc1a2 or version == 0x36dc or version == 0xd5dd or version == 0x299c then
 	print("RBGY JPN game detected")
   base_address = 0xd123
@@ -10,19 +13,20 @@ elseif version == 0xe691 or version == 0xa9d then
 	print("RB USA detected")
   base_address = 0xd163
 elseif version == 0x7c04 then
-	print("Y USA detected")
+	print("Yellow USA detected")
   base_address = 0xd162
 elseif version == 0xd289 or version == 0x9c5e or version == 0xdc5c or version == 0xbc2e or version == 0x4a38 or version == 0xd714 or version == 0xfc7a or version == 0xa456 then
 	print("RB EUR detected")
   base_address = 0xd168
 elseif version == 0x8f4e or version == 0xfb66 or version == 0x3756 or version == 0xc1b7 then
-	print("Y EUR detected")
+	print("Yellow EUR detected")
   base_address = 0xd167
 else
 	print(string.format("unknown version, code: %4x", version))
   print("script stopped")
   return
 end
+        print("Bot in progress, please wait...")
 
 local size = memory.readbyte(base_address)-1
 local dv_addr = (base_address+0x23)+size*0x2C
@@ -57,9 +61,6 @@ while true do
     end
     atkdef = memory.readbyte(dv_addr)
     spespc = memory.readbyte(dv_addr+1)
-    --print(i)
-    print(atkdef) --remove this line for slight speed improvment
-    print(spespc) --remove this line for slight speed improvment
     if shiny(atkdef,spespc) then
         print("Shiny!!! Script stopped.")
         print(string.format("Atk: %d", math.floor(atkdef/16)))
@@ -69,7 +70,6 @@ while true do
         savestate.save(state)
         break
     else
-		print("discarded")
         savestate.load(state)
     end
    
